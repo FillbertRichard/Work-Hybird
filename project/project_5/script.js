c70800c2de7b83bb6a5062993ef55f7140c74f40
FillbertR <86275751+FillbertRichard@users.noreply.github.com>
2022-12-29 17:50:28 +0700

    portfolio

---
+++ project/project_5/script.js
@@ -0,0 +1,4 @@
+function addText() {
+    let button = document.getElementById("confirm");
+    button.getElementsByClassName("name").innerHTML = document.write.getElementsByClassName("card-1-middle");
+}
